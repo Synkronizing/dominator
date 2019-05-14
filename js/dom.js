8d67e110d6d9c47e24f5c853f7d17744aa67a429
Synkronizing <josh6050@gmail.com>
2019-05-14 09:28:51 -0600

    Second Commit: Working DOM - inator final product

--- js/dom.js
+++ js/dom.js
@@ -22,8 +22,45 @@ function create_Ele(){
       if(value){
         element[attribute] = value
       }
-      page.appendChild(element);
+      if(attribute === "parent id" && value){
+        let div = document.getElementById(value);
+        div.appendChild(element);
+      }else{
+        page.appendChild(element);
+      }
+      
     }
+    parent = element.parentNode;
     
     console.log(element);
+}
+
+function remove_Ele(){
+  let last_child = parent.lastChild;
+  last_child.remove();
+}
+
+function reset(){
+  for(let i=0;i<inputs.length;i++){
+    inputs[i].value = "";
+  }
+}
+
+function hide_Ele(){
+  let wrap = document.getElementById('wrap');
+  wrap.className = "d-none";
+  let show_btn = document.createElement('span');
+  show_btn.innerHTML = "Show Panel";
+  show_btn.id = 'sBtn';
+  show_btn.className = "btn btn-primary float-right mt-5";
+  show_btn.setAttribute('onclick',"show_Ele();");
+  page.appendChild(show_btn);
+}
+
+function show_Ele(){
+  let wrap = document.getElementById('wrap');
+  wrap.className = 'd-block';
+  let btn = document.getElementById('sBtn');
+  let p_btn = btn.parentNode;
+  p_btn.removeChild(btn);
 }
